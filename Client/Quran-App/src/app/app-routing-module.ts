9d54bd006873dfb59213e807fa376db949b1183d
MWW1987 <s.mojtaba1987@gmail.com>
2018-09-23 19:33:22 +0330

    Initial Commit

---
+++ Client/Quran-App/src/app/app-routing-module.ts
@@ -0,0 +1,15 @@
+import { AdminComponent } from './admin/admin.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
+import { MainComponent } from './main/main.component';
+
+const routes: Routes = [
+    { path: '', component: MainComponent, pathMatch: 'full'},
+    { path: 'admin', component: AdminComponent}
+];
+
+@NgModule({
+    imports: [ RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ Client/Quran-App/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { MainComponent } from './main/main.component';
+import { AppRoutingModule } from './app-routing-module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
